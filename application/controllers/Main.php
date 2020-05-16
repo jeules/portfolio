1105a3251edf831454209009803e5c4fbad41b23
jeules <julesalmodal@gmail.com>
2020-05-16 11:16:35 +0800

    update - message

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,7 +1,9 @@
 <?php
     class Main extends CI_Controller {
         public function index(){
-            $data['message'] = '<h4 class="display-4">Welcome</h4>';
+            $data['message'] = '<!--<h3 class="display-5">Julius Almodal</h3>--><br/><br/>
+                                <span>Hello, I\'m Jeules. A passionate web developer based in Philippines <br/>
+                                and I will accept assignments worldwide soon!</span><br/>';
             $this->load->view('header');
             $this->load->view('message_view', $data);
             $this->load->view('footer');
--- application/views/message_view.php
+++ application/views/message_view.php
@@ -5,12 +5,30 @@
         background-position: center;
         background-size: cover;
     }
+    img {
+        border-radius: 50%;
+        max-width: 150px;
+        max-height: 150px;
+        margin-bottom: 10px;
+    }
+    .card {
+        padding: 30px;
+        margin-left: 30%;
+        margin-right: 30%;
+        
+        -webkit-box-shadow: -2px -2px 50px -7px rgba(0,0,0,0.54);
+        -moz-box-shadow: -2px -2px 50px -7px rgba(0,0,0,0.54);
+        box-shadow: -2px -2px 50px -7px rgba(0,0,0,0.54);
+    }
 </style>
 
 <div class="container-fluid h-100">
     <div class="row h-100 align-items-center">
         <div class="col-12 text-center">
-            <?php echo $message; ?>
+            <div class="card text-center align-items-center">
+                <img src="<?php echo base_url('assets/media/img_profile.jpg'); ?>">
+                <?php echo $message; ?>
+            </div>
         </div>
     </div>
 </div>
