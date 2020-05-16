3e24c9782019c2465066385410cac20d5d3be1f6
jeules <julesalmodal@gmail.com>
2020-05-16 11:40:50 +0800

    update - message

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,8 +2,8 @@
     class Main extends CI_Controller {
         public function index(){
             $data['message'] = '<!--<h3 class="display-5">Julius Almodal</h3>--><br/><br/>
-                                <span>Hello, I\'m Jeules. A passionate web developer based in Philippines <br/>
-                                and I will accept assignments worldwide soon!</span><br/>';
+                                <span>Hello, I\'m Jeules. A passionate web developer based in Philippines. <br/>
+                                I\'m still working on my contents but thanks for dropping in!</span><br/>';
             $this->load->view('header');
             $this->load->view('message_view', $data);
             $this->load->view('footer');
